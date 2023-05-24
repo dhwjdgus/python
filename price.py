691882945d252b0dc8b0fede449d2792374710ff
dhwjdgus <134288750+dhwjdgus@users.noreply.github.com>
2023-05-24 13:33:07 +0900

    Update price.py

--- price.py
+++ price.py
@@ -1,2 +1,10 @@
-def item_price(shop_file, item):
-  return shopping(shop_file)[item]
+from pathlib import Path
+
+def shopping(shop_file):
+    file_path = Path("data") / shop_file
+    with open(file_path, 'r') as file:
+        result = {}
+        for line in file:
+            item, price = line.strip().split(',')
+            result[item] = int(price)
+        return result
